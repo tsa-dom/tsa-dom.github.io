4d1c9404931d611670ea1055ba2c6a042f866b5d
Tapio Salonen <tsa.dom@pm.me>
2022-03-07 02:02:47 +0200

    Testing

--- config/variables.js
+++ config/variables.js
@@ -1,4 +1,4 @@
-const files = ['blog/index', 'blog/react/theme-toggle', 'blog/seo/react-app-seo-for-github-pages', 'blog/test/test', 'blog/test/test2', 'blog/test/yttest', 'blog/test2/test', 'blog/test2/test2', 'blog/testim']
+const files = []
 
 module.exports = {
   files
